57ded07b14561f11dfe7b0cf9fbc8463075e2825
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-05-11 18:36:51 -0300

    edita settings.py

--- postosaudevirtual/settings.py
+++ postosaudevirtual/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'pages.apps.Pagesconfig',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'postosaudevirtual.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
