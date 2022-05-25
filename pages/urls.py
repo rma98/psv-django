75399a035390dae70c89b07febb639072a0c8242
Francievellyn <fls@a.palmares.ifpe.edu.br>
2022-05-25 10:27:17 -0300

    alteração

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views 
 
+
 app_name = 'pages'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('cadastro', views.CadastroPageView.as_view(), name='cadastro'),
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,28 +1,28 @@
-from django.shortcuts import render, redirect
-from .forms import formMedico
-from .models import Medico
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+class CadastroPageView(TemplateView):
+    template_name = 'cadastroMedico.html'
+
+def index(request):
+    return render(request, "index.html", {})
 
 def cadastroMedico(request):
     if request.method == "POST":
         form = formMedico (request.POST,request.FILES)
         if form.is_valid():
             form.save()
-
     else:
         form = formMedico()
 
-    form = formMedico()
-    return render(request, "cadastroMedico.html", {'form': form})
+    form = formPessoa()
+    return render(request, "cadastro.html", {'form': form})
 
 def all(request):
     nome = ''
     nomeComment = ''
     if request.method == "POST":
-        form = formMedico(request.POST, request.FILES)
+        form = formPessoa(request.POST, request.FILES)
         if form.is_valid():
             nome = form.cleaned_data['name']
-
